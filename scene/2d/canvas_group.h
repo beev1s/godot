b356c2f490c58801e3401f0039e763a2559a46a9
Moritz Burgdorff <mburgdorff@outlook.com>
2025-03-24 10:43:14 +0100

    Added new Control based CanvasGroup

--- scene/2d/canvas_group.h
+++ scene/2d/canvas_group.h
@@ -31,28 +31,98 @@
 #pragma once
 
 #include "scene/2d/node_2d.h"
+#include "scene/gui/control.h"
 
-class CanvasGroup : public Node2D {
-	GDCLASS(CanvasGroup, Node2D)
-	real_t fit_margin = 10.0;
-	real_t clear_margin = 10.0;
-	bool use_mipmaps = false;
+#define CANVAS_GROUP(p_CanvasGroupClassName, p_BaseClass)																											\
+class p_CanvasGroupClassName : public p_BaseClass {																													\
+	GDCLASS(p_CanvasGroupClassName, p_BaseClass)																													\
+	real_t fit_margin = 10.0;																																		\
+	real_t clear_margin = 10.0;																																		\
+	bool use_mipmaps = false;																																		\
+																																									\
+protected:																																							\
+	static void _bind_methods() {																																	\
+		ClassDB::bind_method(D_METHOD("set_fit_margin", "fit_margin"), &p_CanvasGroupClassName::set_fit_margin);													\
+		ClassDB::bind_method(D_METHOD("get_fit_margin"), &p_CanvasGroupClassName::get_fit_margin);																	\
+																																									\
+		ClassDB::bind_method(D_METHOD("set_clear_margin", "clear_margin"), &p_CanvasGroupClassName::set_clear_margin);												\
+		ClassDB::bind_method(D_METHOD("get_clear_margin"), &p_CanvasGroupClassName::get_clear_margin);																\
+																																									\
+		ClassDB::bind_method(D_METHOD("set_use_mipmaps", "use_mipmaps"), &p_CanvasGroupClassName::set_use_mipmaps);													\
+		ClassDB::bind_method(D_METHOD("is_using_mipmaps"), &p_CanvasGroupClassName::is_using_mipmaps);																\
+																																									\
+		ADD_GROUP("Tweaks", "");																																	\
+		ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "fit_margin", PROPERTY_HINT_RANGE, "0,1024,1.0,or_greater,suffix:px"), "set_fit_margin", "get_fit_margin");		\
+		ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "clear_margin", PROPERTY_HINT_RANGE, "0,1024,1.0,or_greater,suffix:px"), "set_clear_margin", "get_clear_margin");	\
+		ADD_PROPERTY(PropertyInfo(Variant::BOOL, "use_mipmaps"), "set_use_mipmaps", "is_using_mipmaps");															\
+	}																																								\
+																																									\
+public:																																								\
+	void set_fit_margin(real_t p_fit_margin)																														\
+	{																																								\
+		ERR_FAIL_COND(p_fit_margin < 0.0);																															\
+																																									\
+		fit_margin = p_fit_margin;																																	\
+		RS::get_singleton()->canvas_item_set_canvas_group_mode(get_canvas_item(), RS::CANVAS_GROUP_MODE_TRANSPARENT, clear_margin, true, fit_margin, use_mipmaps);	\
+																																									\
+		queue_redraw();																																				\
+	}																																								\
+	real_t get_fit_margin() const { return fit_margin; }																											\
+																																									\
+	void set_clear_margin(real_t p_clear_margin)																													\
+	{																																								\
+		ERR_FAIL_COND(p_clear_margin < 0.0);																														\
+																																									\
+		clear_margin = p_clear_margin;																																\
+		RS::get_singleton()->canvas_item_set_canvas_group_mode(get_canvas_item(), RS::CANVAS_GROUP_MODE_TRANSPARENT, clear_margin, true, fit_margin, use_mipmaps);	\
+																																									\
+		queue_redraw();																																				\
+	}																																								\
+	real_t get_clear_margin() const { return clear_margin; }																										\
+																																									\
+	void set_use_mipmaps(bool p_use_mipmaps) {																														\
+		use_mipmaps = p_use_mipmaps;																																\
+		RS::get_singleton()->canvas_item_set_canvas_group_mode(get_canvas_item(), RS::CANVAS_GROUP_MODE_TRANSPARENT, clear_margin, true, fit_margin, use_mipmaps);	\
+	}																																								\
+	bool is_using_mipmaps() const { return use_mipmaps; }																											\
+																																									\
+	p_CanvasGroupClassName() { set_fit_margin(10.0); }																												\
+	~p_CanvasGroupClassName() {																																		\
+		RS::get_singleton()->canvas_item_set_canvas_group_mode(get_canvas_item(), RS::CANVAS_GROUP_MODE_DISABLED);													\
+	}																																								\
+																																									\
+	PackedStringArray get_configuration_warnings() const {																											\
+		PackedStringArray warnings = p_BaseClass::get_configuration_warnings();																							\
+																																									\
+		if (is_inside_tree()) {																																		\
+			bool warned_about_ancestor_clipping = false;																											\
+			bool warned_about_canvasgroup_ancestor = false;																											\
+			Node *n = get_parent();																																	\
+			while (n) {																																				\
+				CanvasItem *as_canvas_item = Object::cast_to<CanvasItem>(n);																						\
+				if (!warned_about_ancestor_clipping && as_canvas_item && as_canvas_item->get_clip_children_mode() != CLIP_CHILDREN_DISABLED) {						\
+					warnings.push_back(vformat(RTR("Ancestor \"%s\" clips its children, so this CanvasGroup will not function properly."), as_canvas_item->get_name()));\
+					warned_about_ancestor_clipping = true;																											\
+				}																																					\
+																																									\
+				p_CanvasGroupClassName *as_canvas_group = Object::cast_to<p_CanvasGroupClassName>(n);																\
+				if (!warned_about_canvasgroup_ancestor && as_canvas_group) {																						\
+					warnings.push_back(vformat(RTR("Ancestor \"%s\" is a type of CanvasGroup, so this type of CanvasGroup will not function properly."), as_canvas_group->get_name()));\
+					warned_about_canvasgroup_ancestor = true;																										\
+				}																																					\
+																																									\
+				/* Only break out early once both warnings have been triggered, so
+				   that the user is aware of both possible reasons for clipping not working.*/																		\
+				if (warned_about_ancestor_clipping && warned_about_canvasgroup_ancestor) {																			\
+					break;																																			\
+				}																																					\
+				n = n->get_parent();																																\
+			}																																						\
+		}																																							\
+																																									\
+		return warnings;																																			\
+	}																																								\
+};																																									\
 
-protected:
-	static void _bind_methods();
-
-public:
-	void set_fit_margin(real_t p_fit_margin);
-	real_t get_fit_margin() const;
-
-	void set_clear_margin(real_t p_clear_margin);
-	real_t get_clear_margin() const;
-
-	void set_use_mipmaps(bool p_use_mipmaps);
-	bool is_using_mipmaps() const;
-
-	virtual PackedStringArray get_configuration_warnings() const override;
-
-	CanvasGroup();
-	~CanvasGroup();
-};
+CANVAS_GROUP(CanvasGroup, Node2D)
+CANVAS_GROUP(ControlCanvasGroup, Control)
--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -892,6 +892,7 @@ void register_scene_types() {
 	/* REGISTER 2D */
 
 	GDREGISTER_CLASS(Node2D);
+	GDREGISTER_CLASS(ControlCanvasGroup);
 	GDREGISTER_CLASS(CanvasGroup);
 	GDREGISTER_CLASS(CPUParticles2D);
 	GDREGISTER_CLASS(GPUParticles2D);
